9ffe616dd0c0001bb127942f91d36ef1b261ded8
adriantomczak <tomczakadrian86@gmail.com>
2019-01-28 12:31:16 +0100

    views & templates

--- elibrary/catalog/views.py
+++ elibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, BookInstance, Author, Genre
 
 
@@ -19,3 +20,13 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 20
+    queryset = Book.objects.all().order_by('author')
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
